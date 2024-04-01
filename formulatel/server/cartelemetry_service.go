0a5e6ac76adbbd1c533adb6ca7cdc0528925c3ae
James Isnor <isnor.james@gmail.com>
2024-04-01 12:26:33 -0300

    generic metric recorder interface

--- formulatel/server/cartelemetry_service.go
+++ formulatel/server/cartelemetry_service.go
@@ -2,55 +2,81 @@ package formulatel
 
 import (
 	"context"
+	"sync/atomic"
 
 	pb "github.com/isnor/formulatel/internal/genproto"
 	"go.opentelemetry.io/otel/metric"
 )
 
-type CarTelemetryMetrics interface {
-	RecordTelemetry(context.Context, *pb.CarTelemetryData)
+// FormulaTelMetricsRecorder describes something that records metrics pertaining to T. Implementations should be
+// thread-safe
+type FormulaTelMetricsRecorder[T any] interface {
+	Record(context.Context, *T)
 }
 
 type CarTelemetryMetricsImpl struct {
 	Gauges *CarTelemetryGauges
 }
 
-// TODO: add attributes to the metrics from the context? I'm not sure how we're going to do this
+// TODO: add attributes to the metrics from the context? I'm not sure how we're going to do this. I think
+// the answer might be _hammering_ values into the context rather than in some singleton struct like MetricsImpl
+// because context is all the asynchronous gauges will be able to use, but the better solution is probably
+// to just write our own exporter
 func (m *CarTelemetryMetricsImpl) RecordTelemetry(ctx context.Context, telemetry *pb.CarTelemetryData) {
-	m.Gauges.Speed = int64(telemetry.Speed)
-	m.Gauges.Break = float64(telemetry.Brake)
-	m.Gauges.Throttle = float64(telemetry.Throttle)
+	m.Gauges.Speed.Store(int64(telemetry.Speed))
+	m.Gauges.Break.Store(float64(telemetry.Brake))
+	m.Gauges.Throttle.Store(float64(telemetry.Throttle))
+}
+
+func (m *CarTelemetryMetricsImpl) Record(ctx context.Context, telemetry *pb.CarTelemetryData) {
+	m.RecordTelemetry(ctx, telemetry)
 }
 
 type CarTelemetryGauges struct {
-	Speed    int64
-	Break    float64
-	Throttle float64
+	Speed    atomic.Int64
+	Break    AtomicFloat32 // float32
+	Throttle AtomicFloat32
 }
 
+type AtomicFloat32 struct {
+	atomic.Value
+}
+
+func (a *AtomicFloat32) Load() float32 {
+	return a.Value.Load().(float32)
+}
+
+// NewGauges creates a new instance of CarTelemetryGauges, which is the in-memory representation of a car's telemetry data.
+// As telemetry comes into the service, CarTelemetryGauges keeps track of the latest value for each sensor of the car.
+// This function registers those gauges with similarly named opentelemetry Gauges
 func NewGauges(meter metric.Meter) *CarTelemetryGauges {
 	gauges := &CarTelemetryGauges{}
 	meter.Int64ObservableGauge("formulatelrpc.speed.gauge", metric.WithInt64Callback(func(ctx context.Context, o metric.Int64Observer) error {
-		o.Observe(gauges.Speed)
+		o.Observe(gauges.Speed.Load())
 		return nil
 	}))
 	meter.Float64ObservableGauge("formulatelrpc.break.gauge", metric.WithFloat64Callback(func(ctx context.Context, fo metric.Float64Observer) error {
-		fo.Observe(gauges.Break)
+		fo.Observe(float64(gauges.Break.Load()))
 		return nil
 	}))
 	meter.Float64ObservableGauge("formulatelrpc.throttle.gauge", metric.WithFloat64Callback(func(ctx context.Context, fo metric.Float64Observer) error {
-		fo.Observe(gauges.Throttle)
+		fo.Observe(float64(gauges.Throttle.Load()))
 		return nil
 	}))
 	return gauges
 }
 
 type CarTelemetryService struct {
-	CarTelemetryMetrics
+	FormulaTelMetricsRecorder[pb.CarTelemetryData]
 	pb.UnimplementedCarTelemetryDataServiceServer
 }
 
 func (c *CarTelemetryService) SendCarTelemetryData(ctx context.Context, data *pb.CarTelemetryData) (*pb.CarTelemetryAck, error) {
-	c.CarTelemetryMetrics.RecordTelemetry(ctx, data)
+	c.FormulaTelMetricsRecorder.Record(ctx, data)
+	return &pb.CarTelemetryAck{}, nil
+}
+
+func SendData[T any](ctx context.Context, recorder FormulaTelMetricsRecorder[T], data *T) (*pb.CarTelemetryAck, error) {
+	recorder.Record(ctx, data)
 	return &pb.CarTelemetryAck{}, nil
 }
--- formulatel/server/server.go
+++ formulatel/server/server.go
@@ -9,25 +9,12 @@ import (
 
 func FormulaTelServer(meter metric.Meter) *grpc.Server {
 
-	// meter := otel.Meter("formulatel")
-	apiCounter, err := meter.Int64Counter(
-		"formulatelrpc.motion.packets",
-		metric.WithDescription("Number of motion type packets received by formulatel"),
-		metric.WithUnit("{packets}"),
-	)
-	if err != nil {
-		panic(err) // TODO: remove this
-	}
 	server := grpc.NewServer(grpc.StatsHandler(otelgrpc.NewServerHandler()))
 	pb.RegisterCarMotionDataServiceServer(server, &CarMotionService{
 		CarMotionMetrics: &CarMotionMetricsImpl{
-			MotionPacketsCounter: apiCounter,
 		},
 	})
-	pb.RegisterCarTelemetryDataServiceServer(server, &CarTelemetryService{
-		CarTelemetryMetrics: &CarTelemetryMetricsImpl{
-			Gauges: NewGauges(meter),
-		},
-	})
+
+	pb.RegisterCarTelemetryDataServiceServer(server, &CarTelemetryService{})
 	return server
 }
