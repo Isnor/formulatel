387731e98ddda77036a35486cee83bd29bf7d81a
James Isnor <isnor.james@gmail.com>
2025-10-13 18:55:07 -0300

    fix: clean ingest; name changes for clarity

--- formulatel/cmd/ingest/main.go
+++ formulatel/cmd/ingest/main.go
@@ -30,8 +30,7 @@ func main() {
 	flag.Parse()
 	serverContext, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
-	// TODO: we probably shouldn't bind to 0.0.0.0 (all interfaces), but I found using 127.0.0.1 didn't work: I didn't receive
-	// 	any telemetry packets
+	// TODO: we probably shouldn't bind to 0.0.0.0
 	conn, err := (&net.ListenConfig{}).ListenPacket(serverContext, "udp4", fmt.Sprintf("0.0.0.0:%d", TelemetryPort))
 	if err != nil {
 		slog.Error("failed listening for UDP packets", "port", TelemetryPort, "error", err.Error())
@@ -45,15 +44,28 @@ func main() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	})))
+	var wg sync.WaitGroup
 
-	reader := &f123.F123PacketReader{
-		Packets:            buffer,      // read and unpack F123 packets, placing them in a data-specific channel
-		VehicleDataChannel: vehicleData, // write vehicle packets as their protobuf representation here
-		MotionDataChannel:  vehicleData,
+	// start reading packets
+	f123Ingestion := &f123.F123PacketReader{
+		Server:       conn,
+		PacketBuffer: buffer, // send all packets here
 	}
 
-	// begin readers - we consume packets from the network, put them in a queue, and have other routines send them somewhere
-	var wg sync.WaitGroup
+	wg.Go(func() {
+		if err := f123Ingestion.Run(serverContext); err != nil {
+			slog.ErrorContext(serverContext, "error reading packets. stopping.", "error", err.Error())
+			cancel()
+		}
+	})
+
+	// transform packets into our telemetry type
+	transformer := &f123.F123PacketTransformer{
+		Packets:            buffer,      // read and unpack F123 packets, placing them in a data-specific channel
+		VehicleDataChannel: vehicleData, // write vehicle packets as their protobuf representation here
+		// MotionDataChannel:  vehicleData,
+	}
+
 	wg.Go(func() {
 		defer func() {
 			if err := recover(); err != nil {
@@ -61,32 +73,12 @@ func main() {
 				slog.Error("something terrible has happened", "error", err)
 			}
 		}()
-		reader.Consume(serverContext)
+		transformer.Consume(serverContext)
 	})
 
 	mqttPublisherCtx, cancel := context.WithCancel(serverContext)
 	defer cancel()
 
-	startMQTTv3Publisher(mqttPublisherCtx, &wg, vehicleData)
-
-	if err != nil {
-		slog.ErrorContext(serverContext, err.Error())
-		os.Exit(1)
-	}
-
-	f123Ingestion := &f123.F123FormulaTelIngest{
-		Server:       conn,
-		Cancel:       cancel,
-		PacketBuffer: buffer, // send all packets here
-	}
-
-	f123Ingestion.Run(serverContext)
-	wg.Wait()
-	slog.InfoContext(serverContext, "shut down succesfully")
-}
-
-// startMQTTv3Publisher reads data from `dataChan` and publishes it to an MQTT topic
-func startMQTTv3Publisher(ctx context.Context, wg *sync.WaitGroup, dataChan <-chan *pb.GameTelemetry) error {
 	// Enable logging by uncommenting the below
 	mqttv3.ERROR = slog.NewLogLogger(slog.NewTextHandler(os.Stderr, nil), slog.LevelError)
 	mqttv3.DEBUG = slog.NewLogLogger(slog.NewTextHandler(os.Stdout, nil), slog.LevelDebug)
@@ -96,33 +88,46 @@ func startMQTTv3Publisher(ctx context.Context, wg *sync.WaitGroup, dataChan <-ch
 	// TODO: make it configurable
 	connectionOptions := mqttv3.NewClientOptions().AddBroker("tcp://localhost:1883")
 	connectionOptions.ClientID = "formulatel_ingest"
+
+	// put our telemetry type on a queue
 	mqttClient, err := formulatel.NewMQTTv3Connection(connectionOptions)
 	if err != nil {
-		return err
+		slog.ErrorContext(serverContext, err.Error())
+		cancel()
+		os.Exit(1)
 	}
 
 	wg.Go(func() {
-		for {
-			select {
-			case <-ctx.Done():
-				slog.InfoContext(ctx, "finished publishing to mqtt")
-				return
-			case data := <-dataChan:
-				protoBytes, err := protojson.Marshal(data)
-				if err != nil {
-					// TODO: handle better
-					slog.ErrorContext(ctx, "mqtt ingest failed serializing a message")
-					continue
-				}
-				slog.DebugContext(ctx, "mqtt ingest read a packet")
-				// TODO: make it configurable
-				if token := mqttClient.Publish("formulatel/vehicledata", 1, false, protoBytes); !token.Wait() || token.Error() != nil {
-					slog.ErrorContext(ctx, "v3 client failed publishing", "error", token.Error())
-				}
-				slog.DebugContext(ctx, "published vehicle data to mqtt topic")
-			}
+		if err := startMQTTv3Publisher(mqttPublisherCtx, mqttClient, vehicleData); err != nil {
+			slog.ErrorContext(mqttPublisherCtx, "mqtt publisher failed", "error", err.Error())
 		}
 	})
 
-	return nil
+	wg.Wait()
+	slog.InfoContext(serverContext, "shut down successfully")
+}
+
+// startMQTTv3Publisher reads data from `dataChan` and publishes it to an MQTT topic
+func startMQTTv3Publisher(ctx context.Context, mqttClient mqttv3.Client, dataChan <-chan *pb.GameTelemetry) error {
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.InfoContext(ctx, "finished publishing to mqtt")
+			return nil
+		case data := <-dataChan:
+			protoBytes, err := protojson.Marshal(data)
+			if err != nil {
+				// TODO: handle better
+				slog.ErrorContext(ctx, "mqtt ingest failed serializing a message")
+				continue
+			}
+			slog.DebugContext(ctx, "mqtt ingest read a packet")
+			// TODO: make it configurable
+			if token := mqttClient.Publish("formulatel/vehicledata", 1, false, protoBytes); !token.Wait() || token.Error() != nil {
+				slog.ErrorContext(ctx, "v3 client failed publishing", "error", token.Error())
+			}
+			slog.DebugContext(ctx, "published vehicle data to mqtt topic")
+		}
+	}
 }
--- formulatel/f123/f123.go
+++ formulatel/f123/f123.go
@@ -16,6 +16,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+// TODO: make this configurable
 const maxPacketSize = 2048 // the largest packet is just 1460 bytes.
 
 // this is fairly EA/codemasters F1-specific
@@ -25,22 +26,28 @@ func ReadBin[T any](reader io.Reader) *T {
 	return x
 }
 
-type F123FormulaTelIngest struct {
+type F123PacketReader struct {
 	Server       net.PacketConn
-	Cancel       context.CancelFunc
 	PacketBuffer chan<- []byte
+
+	finishedWithError error
 }
 
-func (f *F123FormulaTelIngest) Run(serverContext context.Context) {
+// Run tries to read packets until `serverContext` is cancelled. If it is, Run
+// returns nil. If Run returns a non-nil error, subsequent calls to Run return
+// the same error and no packets are read.
+func (f *F123PacketReader) Run(serverContext context.Context) error {
+	if f.finishedWithError != nil {
+		return f.finishedWithError
+	}
 	slog.InfoContext(serverContext, "starting formulatel ingest")
 
 	// listen for packets
 	for {
 		select {
-		// this is our "graceful shutdown" attempt
 		case <-serverContext.Done():
 			slog.InfoContext(serverContext, "formulatel ingest closed")
-			return
+			return nil
 		default:
 			// read a packet of data up to MaxPacketSize bytes from a UDP connection
 			// TODO: there's probably a better way to handle this deadline / avoid blocking on ReadFrom, this is just the first
@@ -63,7 +70,8 @@ func (f *F123FormulaTelIngest) Run(serverContext context.Context) {
 
 			if err != nil && !errors.Is(err, os.ErrDeadlineExceeded) {
 				slog.ErrorContext(serverContext, "failed reading packets:", "error", err)
-				f.Cancel() // cancels the server context and effectively shuts down
+				f.finishedWithError = err
+				return f.finishedWithError
 			}
 		}
 	}
@@ -71,15 +79,15 @@ func (f *F123FormulaTelIngest) Run(serverContext context.Context) {
 
 // this is a game-specific implementation detail that unpacks F123 packets and puts them in a channel
 // specific to the type of telemetry in the packet
-type F123PacketReader struct {
+type F123PacketTransformer struct {
 	Packets            <-chan []byte
 	VehicleDataChannel chan<- *pb.GameTelemetry // a channel for the vehicle data
-	MotionDataChannel  chan<- *pb.GameTelemetry // a channel for motion data
-	capture            bool                     // TODO: remove, just for testing. when set, writes a file for every packet received
+	// MotionDataChannel  chan<- *pb.GameTelemetry // a channel for motion data
+	capture bool // TODO: remove, just for testing. when set, writes a file for every packet received
 }
 
 // Consume reads packets from a buffered channel until the channel is closed or the reader is shutdown
-func (f *F123PacketReader) Consume(ctx context.Context) {
+func (f *F123PacketTransformer) Consume(ctx context.Context) {
 	slog.InfoContext(ctx, "f123 packet reader starting consuming")
 	for {
 		select {
@@ -93,7 +101,7 @@ func (f *F123PacketReader) Consume(ctx context.Context) {
 }
 
 // handlePacket reads a packet header and calls Route on the remaining bytes
-func (f *F123PacketReader) handlePacket(ctx context.Context, packet []byte) {
+func (f *F123PacketTransformer) handlePacket(ctx context.Context, packet []byte) {
 	var clone []byte
 	if f.capture {
 		clone = bytes.Clone(packet) // create a copy of packet to write to a file because we pass ownership of packet to a byte buffer; only for packet capture
@@ -113,51 +121,11 @@ func (f *F123PacketReader) handlePacket(ctx context.Context, packet []byte) {
 
 // Route uses the [PacketType] of `header` to read the bytes from `reader` into the appropriate type.
 // TODO: consider the signature here, it was hacked together initially
-func (f *F123PacketReader) Route(ctx context.Context, header *PacketHeader, data *bytes.Buffer) error {
+func (f *F123PacketTransformer) Route(ctx context.Context, header *PacketHeader, data *bytes.Buffer) error {
 
 	// TODO: create a child context and add tracing
 	// todoContext := ctx
 	switch header.PacketId {
-	case CarMotionPacket:
-		motionArray := ReadBin[[22]CarMotionData](data)
-		motion := motionArray[header.PlayerCarIndex]
-		slog.DebugContext(ctx, "got a motion packet", "data", fmt.Sprintf("%#v", motion))
-		// TODO: need to add the protobuf for MotionData and/or figure out how we're going to handle this.
-		t := &pb.GameTelemetry{
-			Title: pb.GameTitle_GAME_TITLE_F123,
-			Data: &pb.GameTelemetry_VehicleData{
-				VehicleData: &pb.VehicleData{
-					// TODO: very much incorrect
-					Steering: motion.WorldPositionX,
-				},
-			},
-		}
-		f.MotionDataChannel <- t
-		// if f.CarMotionDataServiceClient != nil {
-		// 	_, err := f.CarMotionDataServiceClient.SendCarMotionData(todoContext, &f123.CarMotionData{
-		// 		WorldPositionX:     motion.WorldPositionX,
-		// 		WorldPositionY:     motion.WorldPositionY,
-		// 		WorldPositionZ:     motion.WorldPositionZ,
-		// 		WorldVelocityX:     motion.WorldVelocityX,
-		// 		WorldVelocityY:     motion.WorldVelocityY,
-		// 		WorldVelocityZ:     motion.WorldVelocityZ,
-		// 		WorldForwardDirX:   int32(motion.WorldForwardDirX),
-		// 		WorldForwardDirY:   int32(motion.WorldForwardDirY),
-		// 		WorldForwardDirZ:   int32(motion.WorldForwardDirZ),
-		// 		WorldRightDirX:     int32(motion.WorldRightDirX),
-		// 		WorldRightDirY:     int32(motion.WorldRightDirY),
-		// 		WorldRightDirZ:     int32(motion.WorldRightDirZ),
-		// 		GForceLateral:      motion.GForceLateral,
-		// 		GForceLongitudinal: motion.GForceLongitudinal,
-		// 		GForceVertical:     motion.GForceVertical,
-		// 		Yaw:                motion.Yaw,
-		// 		Pitch:              motion.Pitch,
-		// 		Roll:               motion.Roll,
-		// 	})
-		// 	if err != nil {
-		// 		fmt.Println(fmt.Errorf("oh no, an error %s", err))
-		// 	}
-		// }
 	case CarTelemetryPacket:
 		telemetryArray := ReadBin[[22]CarTelemetryData](data)
 		playerTelemetry := telemetryArray[header.PlayerCarIndex]
