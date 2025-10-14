9ce3276adc961951d34d87c2add43b8233fdb91e
James Isnor <isnor.james@gmail.com>
2025-10-13 22:43:59 -0300

    fix: remove mqtt v5

--- formulatel/cmd/ingest/mqtt_v3.go
+++ formulatel/cmd/ingest/mqtt_v3.go
@@ -1,13 +1,16 @@
-package formulatel
+package main
 
 // utilities for v3 mqtt connection
 // https://github.com/eclipse-paho/paho.mqtt.golang/blob/master/cmd/docker/publisher/main.go
 
 import (
+	"context"
 	"log/slog"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	pb "github.com/isnor/formulatel/internal/genproto"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 // NewMQTTv3Connection creates an mqtt client that can be used to publish and subscribe
@@ -28,13 +31,13 @@ func NewMQTTv3Connection(opts *mqtt.ClientOptions) (mqtt.Client, error) {
 
 	// Log events
 	opts.OnConnectionLost = func(cl mqtt.Client, err error) {
-		slog.Error("connection lost")
+		slog.Error("mqtt: connection lost")
 	}
 	opts.OnConnect = func(mqtt.Client) {
-		slog.Info("connected to broker", "broker", opts.Servers)
+		slog.Info("mqtt: connected to broker", "broker", opts.Servers)
 	}
 	opts.OnReconnecting = func(mqtt.Client, *mqtt.ClientOptions) {
-		slog.Error("attempting to reconnect")
+		slog.Error("mqtt: attempting to reconnect")
 	}
 
 	client := mqtt.NewClient(opts)
@@ -43,4 +46,29 @@ func NewMQTTv3Connection(opts *mqtt.ClientOptions) (mqtt.Client, error) {
 	}
 
 	return client, nil
+}
+
+// StartMQTTv3Publisher reads data from `dataChan` and publishes it to an MQTT topic
+func StartMQTTv3Publisher(ctx context.Context, mqttClient mqtt.Client, dataChan <-chan *pb.GameTelemetry) error {
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.InfoContext(ctx, "finished publishing to mqtt")
+			return nil
+		case data := <-dataChan:
+			protoBytes, err := protojson.Marshal(data)
+			if err != nil {
+				// TODO: handle better
+				slog.ErrorContext(ctx, "mqtt ingest failed serializing a message")
+				continue
+			}
+			slog.DebugContext(ctx, "mqtt ingest read a packet")
+			// TODO: make it configurable
+			if token := mqttClient.Publish("formulatel/vehicledata", 1, false, protoBytes); !token.Wait() || token.Error() != nil {
+				slog.ErrorContext(ctx, "v3 client failed publishing", "error", token.Error())
+			}
+			slog.DebugContext(ctx, "published vehicle data to mqtt topic")
+		}
+	}
 }
