f1590f5ee863d02d75fb4000db056d0d72a3ff13
James Isnor <isnor.james@gmail.com>
2025-10-16 20:09:29 -0300

    feat: generate telemetry of car increasing speed

--- formulatel/cmd/gentel/main.go
+++ formulatel/cmd/gentel/main.go
@@ -5,18 +5,36 @@ package main
 import (
 	"context"
 	"flag"
+	"log/slog"
 	"os"
 	"os/signal"
+
+	"github.com/isnor/formulatel/internal/genproto"
 )
 
 var frequency = flag.Int("frequency", 1, "frequency at which to generate telemetry; how many to generate per second")
-var telemetryType = flag.String("type", "vehicle-data", "the type of telemetry to generate")
+var maxGear = flag.Int("max-gear", 7, "max gear of car")
+var maxRPM = flag.Int("max-rpm", 10000, "max RPM of the car")
+var acceleration = flag.Float64("acceleration", 1.0, "how fast we are accelerating")
+var maxSpeed = flag.Int("max-kph", 250, "max speed of the car in k/m")
 
+// var telemetryType = flag.String("type", "vehicle-data", "the type of telemetry to generate")
 
 func main() {
 	flag.Parse()
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
-
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})))
+	tg := NewTelemetryGenerator(*frequency)
+	tg.MaxGear = uint32(*maxGear)
+	tg.MaxRPM = uint32(*maxRPM)
+	tg.MaxSpeed = float32(*maxSpeed)
+	tg.SpeedRate = float32(*acceleration)
+	tg.GenerateLoop(ctx, func(t *genproto.VehicleData) {
+		// TODO: put on mqtt topic
+		slog.InfoContext(ctx, "vroom vroom", "gear", t.Gear, "speed", t.Speed, "rpm", t.Rpm, "temp", t.EngineTemperature)
+	})
 	<-ctx.Done()
 }
--- formulatel/cmd/gentel/telemetry_generator.go
+++ formulatel/cmd/gentel/telemetry_generator.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"time"
 
 	pb "github.com/isnor/formulatel/internal/genproto"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type TelemetryGenerator struct {
-	MaxGear   int
-	MaxRPM    int
-	RPMRate   int
+	MaxGear   uint32
+	MaxRPM    uint32
+	RPMRate   uint32
 	MaxSpeed  float32
-	SpeedRate int // how much the speed goes up per tick
+	SpeedRate float32 // how much the speed goes up per tick
+
+	frequency int // how many ticks per second
 	ticker    *time.Ticker
 }
 
@@ -21,61 +23,71 @@ func NewTelemetryGenerator(frequency int) *TelemetryGenerator {
 	if frequency <= 0 {
 		frequency = 1
 	}
-	t := time.NewTicker(time.Duration(frequency) / time.Second)
+	t := time.NewTicker(time.Second / time.Duration(frequency))
 	return &TelemetryGenerator{
 		ticker: t,
+		frequency: frequency,
 	}
 }
 
-func (g *TelemetryGenerator) GenerateLoop(ctx context.Context, handleFunc func(t *pb.GameTelemetry)) error {
+func (g *TelemetryGenerator) GenerateLoop(ctx context.Context, handleFunc func(t *pb.VehicleData)) error {
 	// this is the initial state
-	x := &pb.GameTelemetry{
-		Title:     pb.GameTitle_GAME_TITLE_UNKNOWN,
-		SessionId: "0000000000",
-		UserId:    "9999999999",
-		Timestamp: timestamppb.Now(),
-		Data: &pb.GameTelemetry_VehicleData{
-			VehicleData: &pb.VehicleData{
-				// let's say we're going to simulate a car speeding up. as time goes up, the speed goes up along
-				// with RPM. eventually the RPM hits a limit and the gear increases. eventually the gears, speed,
-				// and RPM reach a limit
-				Speed:             0,
-				Rpm:               1000,
-				Throttle:          0,
-				Break:             0,
-				Steering:          0,
-				Gear:              1,
-				EngineTemperature: 100,
-				Tires: &pb.VehicleData_Tires{
-					FrontLeft:  &pb.TireData{},
-					FrontRight: &pb.TireData{},
-					BackLeft:   &pb.TireData{},
-					BackRight:  &pb.TireData{},
-				},
-			},
+	previousTel := &pb.VehicleData{
+		// let's say we're going to simulate a car speeding up. as time goes up, the speed goes up along
+		// with RPM. eventually the RPM hits a limit and the gear increases. eventually the gears, speed,
+		// and RPM reach a limit
+		Speed:             0,
+		Rpm:               1000,
+		Throttle:          0,
+		Break:             0,
+		Steering:          0,
+		Gear:              1,
+		EngineTemperature: 100,
+		Tires: &pb.VehicleData_Tires{
+			FrontLeft:  &pb.TireData{},
+			FrontRight: &pb.TireData{},
+			BackLeft:   &pb.TireData{},
+			BackRight:  &pb.TireData{},
 		},
 	}
+	previousTickTime := <-g.ticker.C
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
-		case <-g.ticker.C:
-			handleFunc(x)
+		case now := <-g.ticker.C:
+			// TODO: can we make this more confusing if we use recursion?
+			nextTel := g.NextTelemetry(previousTickTime, previousTel)
+			handleFunc(nextTel)
+			previousTickTime = now
+			previousTel = nextTel
 		}
 	}
 }
 
-func (g *TelemetryGenerator) NextTelemetry(start time.Time, previous *pb.VehicleData) *pb.VehicleData {
+func (g *TelemetryGenerator) NextTelemetry(last time.Time, previous *pb.VehicleData) *pb.VehicleData {
 	var x = *previous
-	if previous.Gear == int32(g.MaxGear) {
-		if previous.Rpm >= uint32(g.MaxRPM) {
-			if previous.Speed >= uint32(g.MaxSpeed) {
-				x.EngineTemperature += 10
-				return &x
-			} else {
-				x.Speed += uint32(g.SpeedRate)
-			}
-		}
+	var timeSinceLast = time.Since(last)
+	slog.Debug("creating new telemetry", "time-since-last", timeSinceLast)
+
+	if previous.Speed >= uint32(g.MaxSpeed) && previous.Rpm >= g.MaxRPM && previous.Gear >= int32(g.MaxGear) {
+		// we're at the limit, just increase the temperature
+		x.EngineTemperature += 10
+		return &x
 	}
-	return nil
+	// bit annoying that we can only increase the speed by steps of 1, maybe we should change this
+	speedIncrease := uint32(g.SpeedRate * float32(timeSinceLast.Milliseconds()) / float32(g.frequency))
+	slog.Debug("increased speed", "increase", speedIncrease, "speed_rate", g.SpeedRate, "tsl", timeSinceLast.Seconds())
+	x.Speed += speedIncrease
+	x.EngineTemperature += 2
+
+	if previous.Rpm < g.MaxRPM {
+		x.Rpm += speedIncrease * 100
+	} else if x.Gear != int32(g.MaxGear) {
+		x.Gear += 1
+		x.Rpm -= 9000
+		x.Speed -= 10
+	}
+
+	return &x
 }
