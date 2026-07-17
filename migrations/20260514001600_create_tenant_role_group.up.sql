b61a7ba227a4273228f52322bf259d07ba540a6e
James Isnor <isnor.james@gmail.com>
2026-07-16 21:10:46 -0300

    fix: fix failing migration tests

--- migrations/20260514001600_create_tenant_role_group.up.sql
+++ migrations/20260514001600_create_tenant_role_group.up.sql
@@ -1,6 +1,12 @@
 -- the telemetry_readers role represents a "group" of users who can read telemetry.
 -- all of the tenant roles are part of this group
-CREATE ROLE telemetry_readers WITH NOLOGIN;
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'telemetry_readers') THEN
+    CREATE ROLE telemetry_readers WITH NOLOGIN;
+  END IF;
+END
+$$;
 GRANT USAGE ON SCHEMA telemetry TO telemetry_readers;
 GRANT SELECT ON ALL TABLES IN SCHEMA telemetry TO telemetry_readers;
 ALTER DEFAULT PRIVILEGES IN SCHEMA telemetry GRANT SELECT ON TABLES TO telemetry_readers;
